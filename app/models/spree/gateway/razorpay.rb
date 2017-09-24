d15729ca85e3bc9d998ca6fd7c9a6656c87bf627
Mohd Sameer <mohdsameer1310@gmail.com>
2017-09-24 17:48:53 +0530

    Added razorpay

---
+++ app/models/spree/gateway/razorpay.rb
@@ -0,0 +1,36 @@
+module Spree
+  class Gateway::Razorpay < Gateway
+    preference :key_id, :string
+    preference :secret_key, :string
+
+    def provider
+      self
+    end 
+
+    def supports?(source)
+      true
+    end
+
+    def auto_capture?
+      true
+    end
+
+    def method_type
+      'razorpay'
+    end
+
+    def purchase(amount, express_checkout, gateway_options={})
+      if true
+        Class.new do
+          def success?; true; end
+          def authorization; nil; end
+        end.new
+      else
+      end
+    end
+
+    def refund(payment, amount)
+       puts 'In Refund'
+    end
+  end
+end
